afe429b1628a4ea055dd228af451798aa5de97cf
huangxin <huangxin@sensetime.com>
2021-06-18 19:59:43 +0800

    add AttnBlock to attnhead

--- mmseg/models/decode_heads/segformer_AttnHead.py
+++ mmseg/models/decode_heads/segformer_AttnHead.py
@@ -4,11 +4,14 @@
 # This work is licensed under the NVIDIA Source Code License
 # ---------------------------------------------------------------
 import numpy as np
+from timm.models.vision_transformer import PatchEmbed
 import torch.nn as nn
 import torch
+import math
 from mmcv.cnn import ConvModule, DepthwiseSeparableConvModule
 from collections import OrderedDict
 
+from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 from mmseg.ops import resize
 from ..builder import HEADS
 from .decode_head import BaseDecodeHead
@@ -16,6 +19,111 @@ from mmseg.models.utils import *
 #import attr
 
 from IPython import embed
+
+class MLP(nn.Module):
+    """
+    Linear Embedding
+    """
+    def __init__(self, input_dim=2048, embed_dim=768):
+        super().__init__()
+        self.proj = nn.Linear(input_dim, embed_dim)
+
+    def forward(self, x):
+        x = x.flatten(2).transpose(1, 2)
+        x = self.proj(x)
+        return x
+
+class DWConv(nn.Module):
+    def __init__(self, dim=768):
+        super(DWConv, self).__init__()
+        self.dwconv = nn.Conv2d(dim, dim, 3, 1, 1, bias=True, groups=dim)
+
+    def forward(self, x, H, W):
+        B, N, C = x.shape
+        x = x.transpose(1, 2).view(B, C, H, W)
+        x = self.dwconv(x)
+        x = x.flatten(2).transpose(1, 2)
+
+        return x
+
+class OverlapPatchEmbed(nn.Module):
+    """ Image to Patch Embedding
+    """
+
+    def __init__(self, img_size=224, patch_size=7, stride=4, in_chans=3, embed_dim=768):
+        super().__init__()
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
+
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.H, self.W = img_size[0] // patch_size[0], img_size[1] // patch_size[1]
+        self.num_patches = self.H * self.W
+        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=stride,
+                              padding=(patch_size[0] // 2, patch_size[1] // 2))
+        self.norm = nn.LayerNorm(embed_dim)
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.proj(x)
+        _, _, H, W = x.shape
+        x = x.flatten(2).transpose(1, 2)
+        x = self.norm(x)
+
+        return x, H, W
+
+class Mlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.dwconv = DWConv(hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+
+    def forward(self, x, H, W):
+        x = self.fc1(x)
+        x = self.dwconv(x, H, W)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
 
 class Attention(nn.Module):
     def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
@@ -62,6 +170,7 @@ class Block(nn.Module):
         x = x + self.drop_path(self.mlp(self.norm2(x)))
         return x
 
+
 @HEADS.register_module()
 class SegFormerAttnHead(BaseDecodeHead):
     """
@@ -78,15 +187,17 @@ class SegFormerAttnHead(BaseDecodeHead):
         decoder_params = kwargs['decoder_params']
         embedding_dim = decoder_params['embed_dim']
 
-        #self.linear_c4 = MLP(input_dim=c4_in_channels, embed_dim=embedding_dim)
-        #self.linear_c3 = MLP(input_dim=c3_in_channels, embed_dim=embedding_dim)
-        #self.linear_c2 = MLP(input_dim=c2_in_channels, embed_dim=embedding_dim)
-        #self.linear_c1 = MLP(input_dim=c1_in_channels, embed_dim=embedding_dim)
+        self.linear_c4 = MLP(input_dim=c4_in_channels, embed_dim=embedding_dim)
+        self.linear_c3 = MLP(input_dim=c3_in_channels, embed_dim=embedding_dim)
+        self.linear_c2 = MLP(input_dim=c2_in_channels, embed_dim=embedding_dim)
+        self.linear_c1 = MLP(input_dim=c1_in_channels, embed_dim=embedding_dim)
 
-        self.attn1 = Attention(dim = c1_in_channels)
-        self.attn2 = Attention(dim = c2_in_channels)
-        self.attn3 = Attention(dim = c3_in_channels)
-        self.attn4 = Attention(dim = c4_in_channels)
+        #self.patch_embed1 = OverlapPatchEmbed(img_size=img_size // 4, patch_size=3, stride=2, in_chans=embed_dims[0],embed_dim=embed_dims[1])
+
+        #self.attn1 = Attention(dim = c1_in_channels)
+        #self.attn2 = Attention(dim = c2_in_channels)
+        #self.attn3 = Attention(dim = c3_in_channels)
+        #self.attn4 = Attention(dim = c4_in_channels)
 
         self.linear_fuse = ConvModule(
             in_channels=embedding_dim*4,
@@ -104,20 +215,26 @@ class SegFormerAttnHead(BaseDecodeHead):
         ############## MLP decoder on C1-C4 ###########
         n, _, h, w = c4.shape
 
-        _c4 = self.attn4(c4).permute(0,2,1).reshape(n, -1, c4.shape[2], c4.shape[3])
+        _c4 = self.linear_c4(c4).permute(0,2,1).reshape(n, -1, c4.shape[2], c4.shape[3])
         _c4 = resize(_c4, size=c1.size()[2:],mode='bilinear',align_corners=False)
 
-        _c3 = self.attn3(c3).permute(0,2,1).reshape(n, -1, c3.shape[2], c3.shape[3])
+        _c3 = self.linear_c3(c3).permute(0,2,1).reshape(n, -1, c3.shape[2], c3.shape[3])
         _c3 = resize(_c3, size=c1.size()[2:],mode='bilinear',align_corners=False)
 
-        _c2 = self.attn2(c2).permute(0,2,1).reshape(n, -1, c2.shape[2], c2.shape[3])
+        _c2 = self.linear_c2(c2).permute(0,2,1).reshape(n, -1, c2.shape[2], c2.shape[3])
         _c2 = resize(_c2, size=c1.size()[2:],mode='bilinear',align_corners=False)
 
-        _c1 = self.attn1(c1).permute(0,2,1).reshape(n, -1, c1.shape[2], c1.shape[3])
+        _c1 = self.linear_c1(c1).permute(0,2,1).reshape(n, -1, c1.shape[2], c1.shape[3])
 
         _c = self.linear_fuse(torch.cat([_c4, _c3, _c2, _c1], dim=1))
 
+        patch_embed =  OverlapPatchEmbed(img_size=c1.size(), patch_size=3, stride=2, in_chans=x.shape[1],embed_dim=x.shape[1])
+        x, H, W = patch_embed(_c)
+        block1 = Block(dim=x.shape[1], num_heads=12)
+        x = block1(x)
+        x = x.reshape(n, H, W, -1).permute(0, 3, 1, 2).contiguous()
         x = self.dropout(_c)
         x = self.linear_pred(x)
 
+        
         return x
